dd8515cf0435fc9cb4696e59f9e7c4b80d42de7a
Ángel Castellanos <141193208+Castamor@users.noreply.github.com>
2023-10-23 14:50:18 -0700

    modify fetchAPI

--- src/services/consultaAPI.jsx
+++ src/services/consultaAPI.jsx
@@ -2,27 +2,30 @@ import { fetchAPI } from './fetch'
 import { NADA, VACIO } from '../constant/constantes'
 
 export const consultaAPI = async (datos, setLoading) => {
-    setLoading(true)
-    let url =
-    datos.pais
-        ? `https://api.openweathermap.org/geo/1.0/direct?q=${datos.ciudad},${datos.pais}&limit=1&appid=${import.meta.env.VITE_API_KEY}`
-        : `https://api.openweathermap.org/geo/1.0/direct?q=${datos.ciudad}&limit=1&appid=${import.meta.env.VITE_API_KEY}`
+    try {
+        setLoading(true)
+        const urlDatosGenerales = `https://api.openweathermap.org/geo/1.0/direct?q=${datos.ciudad},${datos.pais || ''}&limit=1&appid=${import.meta.env.VITE_API_KEY}`
 
-    const datosGenerales = await fetchAPI(url)
+        const datosGenerales = await fetchAPI(urlDatosGenerales)
+        if (datosGenerales.length === VACIO) {
+            setLoading(false)
+            return NADA
+        }
 
-    if (datosGenerales.length === VACIO) {
+        const { lat, lon, state } = datosGenerales[0]
+
+        const urlClimaActual = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&lang=es&units=metric`
+        const urlClimaPrevisto = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&lang=es&units=metric`
+
+        const [climaActual, climaPrevisto] = await Promise.all([
+            fetchAPI(urlClimaActual),
+            fetchAPI(urlClimaPrevisto)
+        ])
+
+        setLoading(false)
+        return { estado: state, climaActual, climaPrevisto }
+    } catch (error) {
         setLoading(false)
         return NADA
     }
-
-    const { lat, lon, state } = datosGenerales[0]
-    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&lang=es&units=metric`
-
-    const data = await fetchAPI(url)
-    const { timezone, name } = data
-    const { temp, feels_like, humidity } = data.main
-    const { description, main, icon } = data.weather[0]
-
-    setLoading(false)
-    return { name, state, temp, feels_like, humidity, description, main, timezone, icon }
 }
